4b7ca61d76fa03dacf6455b83211de43ffb14bec
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-23 19:34:32 +0200

    sm6250-common: Initial variant handler libinit
    * This libinit has been made to commonize device variants props handling
    
    [dereference23: Adapt for Xiaomi SM6250]
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    Change-Id: Iab68ff451ab1d6e861fb4cda4ef07fad3123ecde

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -25,3 +25,7 @@ TARGET_2ND_CPU_VARIANT := cortex-a76
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
+
+# Init
+TARGET_INIT_VENDOR_LIB ?= //$(COMMON_PATH):init_xiaomi_atoll
+TARGET_RECOVERY_DEVICE_MODULES ?= init_xiaomi_atoll
