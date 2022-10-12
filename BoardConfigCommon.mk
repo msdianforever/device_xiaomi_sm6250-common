723d5a6a6e7b036f183a21c20a051c50c3713bbc
Edgars Cīrulis <edgarsscirulis@gmail.com>
2022-10-12 19:15:32 +0300

    sm6250-common: Add framework compatibility matrix
    Signed-off-by: Edgars Cīrulis <edgarsscirulis@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,3 +20,8 @@ TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a76
+
+# HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/device_framework_matrix.xml
+DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
